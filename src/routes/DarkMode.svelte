072796b7afc86d5f1385f2b0bb7b914eae8a10bc
swyx <shawnthe1@gmail.com>
2022-11-02 04:38:55 -0700

    add darkmode

---
+++ src/routes/DarkMode.svelte
@@ -0,0 +1,62 @@
+<script>
+	let isDark = false;
+	if (typeof localStorage !== 'undefined') {
+		if (
+			localStorage.theme === 'dark' ||
+			(!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
+		) {
+			isDark = true;
+		}
+	}
+	function toggleDarkMode() {
+		if (isDark) {
+			document.documentElement.classList.remove('dark');
+			localStorage.theme = 'light';
+			isDark = false;
+		} else {
+			document.documentElement.classList.add('dark');
+			localStorage.theme = 'dark';
+			isDark = true;
+		}
+	}
+</script>
+
+<button
+aria-label="Toggle Dark Mode"
+class="fixed top-8 right-8 ml-1 flex h-9 w-9 items-center justify-center rounded-lg bg-yellow-400 ring-yellow-400
+transition-all hover:ring-2 dark:bg-yellow-800"
+on:click={toggleDarkMode}
+>
+{#if isDark}
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    class="h-5 w-5 text-gray-800 dark:text-yellow-100"
+  >
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728
+      0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+    />
+  </svg>
+{:else}
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    class="h-5 w-5 text-gray-800 dark:text-gray-200"
+  >
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+    />
+  </svg>
+{/if}
+</button>
--- src/routes/shared.js
+++ src/routes/shared.js
@@ -1,4 +1,47 @@
-import { writable } from "svelte/store";
+// import { writable } from "svelte/store";
+import { writable, readable } from 'svelte/store';
 
 export const showLogos = writable(false);
 export const showInternalConnectors = writable(false);
+
+// let stores = new Map();
+
+const breakpoints = ['xs', 'sm', 'md', 'lg', 'xl', 'xxl']
+export function breakPointStore() {
+  if (typeof window === 'undefined') {
+    return writable(false);
+  }
+  const sizes = {
+    xs: window.matchMedia('(max-width: 639px)'),
+    sm: window.matchMedia('(min-width: 640px)'),
+    md: window.matchMedia('(min-width: 768px)'),
+    lg: window.matchMedia('(min-width: 1024px)'),
+    xl: window.matchMedia('(min-width: 1280px)'),
+    xxl: window.matchMedia('(min-width: 1536px)'),
+  }
+
+  let currentMatch = null
+  for (const size of breakpoints) {
+    if (sizes[size].matches) {
+      currentMatch = size
+      break;
+    }
+  }
+
+  let store = readable(currentMatch, (set) => {
+    Object.entries(sizes).forEach(([key, listener]) => {
+      listener.addEventListener('change',
+        (value) => set(value.matches ? key : breakpoints[breakpoints.indexOf(key) - 1])
+      );
+    })
+    return () => {
+      Object.entries(sizes).forEach(([key, listener]) => {
+        listener.removeEventListener('change',
+          (value) => set(value.matches ? key : breakpoints[breakpoints.indexOf(key) - 1])
+        );
+      })
+    }
+  });
+
+  return store
+}
